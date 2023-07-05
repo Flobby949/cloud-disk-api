af404ae63a790d677fc7a98c1097dcabb6ff9516
jinchenxing <2541226493@qq.com>
2023-07-05 23:51:14 +0800

    egg.js 基础入门

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,18 @@ const { Controller } = require('egg');
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    ctx.body = '全栈开发工程师';
+  }
+
+  async list() {
+    this.ctx.body = {
+      msg: 'ok',
+      data: [
+        { name: '微服务技术', price: 100 },
+        { name: '容器化技术', price: 88 },
+        { name: 'Flutter 开发', price: 66 },
+      ],
+    };
   }
 }
 
--- config/config.default.js
+++ config/config.default.js
@@ -10,13 +10,22 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1688569906366_9611';
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = ['errorHandler'];
+
+  config.security = {
+    // 关闭 csrf
+    csrf: { enable: false },
+    // 跨域白名单
+    domainWhiteList: ['http://localhost:3000'],
+  };
+  // 允许跨域的方法
+  config.cors = { origin: '*', allowMethods: 'GET, PUT, POST, DELETE, PATCH' };
 
   // add your user config here
   const userConfig = {
