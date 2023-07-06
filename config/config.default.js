90df8e3bf932c77fb2ae9877a503558efedde52c
jinchenxing <2541226493@qq.com>
2023-07-06 13:57:11 +0800

    Sequelize数据库配置初始化

--- config/config.default.js
+++ config/config.default.js
@@ -27,6 +27,29 @@ module.exports = appInfo => {
   // 允许跨域的方法
   config.cors = { origin: '*', allowMethods: 'GET, PUT, POST, DELETE, PATCH' };
 
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    username: 'root',
+    password: 'kobe24',
+    port: 3306,
+    database: 'cloud_disk',
+    // 中国时区
+    timezone: '+08:00',
+    define: {
+      // 取消数据表名复数
+      freezeTableName: true,
+      // 自动写入时间戳 created_at updated_at
+      timestamps: true, // 字段生成软删除时间戳 deleted_at
+      // paranoid: true,
+      createdAt: 'created_time',
+      updatedAt: 'updated_time',
+      // deletedAt: 'deleted_time',
+      // 所有驼峰命名格式化
+      underscored: true,
+    },
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
